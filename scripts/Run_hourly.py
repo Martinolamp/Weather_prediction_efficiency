89c1ff4ec44211e89ca9a66b114c72b76ae95644
crazytestuser123 <mariusz.nowacki32@op.pl>
2026-01-19 13:08:56 +0100

    Small modification

--- scripts/Run_hourly.py
+++ scripts/Run_hourly.py
@@ -1,14 +1,17 @@
 from Main import current_weather_data,fetch_cities_from_db
 from Supabase_operatoins import SupbaseConnection
 
-if __name__ == "__main__":
-        cities=fetch_cities_from_db("Cities")
-          
-        for city_id, city_name in cities.items():
-            try:
-                #print(city_id,city_name)
-                current_weather_data(city_name,city_id)
-            except Exception as e:
-                print(f" Błąd podczas przetwarzania miasta {city_name}: {e}")
+
+def run():
+    cities=fetch_cities_from_db("Cities")
+        
+    for city_id, city_name in cities.items():
+        try:
+            #print(city_id,city_name)
+            current_weather_data(city_name,city_id)
+        except Exception as e:
+            print(f" Błąd podczas przetwarzania miasta {city_name}: {e}")
 
  
+if __name__ == "__main__":
+    run() # To jest kluczowe! Bez tego skrypt się nie odpali.
